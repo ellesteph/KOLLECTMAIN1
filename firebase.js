6f0d0863a800955fdbfb2608f6e449de4a2d929b
ellesteph <ellesteph2@gmail.com>
2026-03-24 22:19:46 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,4 @@
 // firebase.js
-import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { getFirestore, doc, setDoc } from "firebase/firestore";
-
 const firebaseConfig = {
   apiKey: "AIzaSyBLN0O36j1DWxUN5U1B4iuP6kH-YNUENkk",
   authDomain: "kollect-aa5a3.firebaseapp.com",
@@ -12,9 +8,3 @@ const firebaseConfig = {
   appId: "1:57011462366:web:a060ffe0e8be47f61ae24f",
   measurementId: "G-QCP7K46FXY"
 };
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
